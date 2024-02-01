4e9cf48e8a4e282ba7247e00dbe24d0c5583a271
Marjo <93marjo.ek@gmail.com>
2024-02-01 09:21:34 +0200

    Arvaus tehtävä

--- App.js
+++ App.js
@@ -1,11 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, Alert} from 'react-native';
 
 export default function App() {
+
+  const[numb, setNumb] = useState (0);
+  const[text, setText] = useState('Make a guess between 1-100');
+  const [guessCount, setGuessCount] = useState(0);
+  
+
+  const checkGuess = () => {
+    const guess = Math.floor(Math.random() * 100) + 1;
+
+    setGuessCount(guessCount + 1);
+    
+
+    if (numb === guess) {
+      Alert.alert("Arvasit oikein")
+      setGuessCount(0);
+    }else if(numb< guess){
+      setText(`Your guess ${numb} was too low.`);
+    } else {
+      setText(`Your guess ${numb} was too high.`);
+    } 
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>
+        {text}
+        </Text>
+        <TextInput
+        keyboardType='numeric'
+        style={{fontSize:18, width:200, borderColor: 'gray', borderWidth:1}}
+      onChangeText={numb => setNumb(numb)}
+      value={numb}/>
+
+      <Button onPress={checkGuess} title="Make a guess" />
+
     </View>
   );
 }
